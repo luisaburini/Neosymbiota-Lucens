b489e9570facf5b31832a0ede0a1e049013ef487
Luisa Burini <lburini@unicamp.br>
2025-07-16 00:32:44 -0300

    conserta partes do codigo e documentacao

--- README.md
+++ README.md
@@ -9,9 +9,10 @@ Instruções de montagem para o trabalho Neosymbiota lucens de Licida Vidal.
 Integração de Arduino UNO com
 
 * Placa solar: [ref 1](https://www.paraisodosbits.com.br/2016/11/09/usando-o-arduino-com-painel-solar/), [ref 2](https://www.instructables.com/Self-Sufficient-Arduino-Board/), [ref 3](https://docs.arduino.cc/learn/electronics/power-pins/)
+* Motores DC com controlador L293D [ref 1](https://lastminuteengineers.com/l293d-dc-motor-arduino-tutorial/)
 * Bomba aquática submersível: [ref 1](https://www.youtube.com/watch?v=HBGYIuBC3BQ), [ref 2](https://www.youtube.com/watch?v=yZqnqxHI59Q)
 * Sensor supersônico para medir distância: [ref 1](https://portal.vidadesilicio.com.br/hc-sr04-sensor-ultrassonico/), [ref 2](https://www.blogdarobotica.com/2025/01/29/como-utilizar-o-sensor-ultrassonico-de-distancia-jsn-sr04t-aj-sr04m-com-arduino/), [ref 3](https://www.usinainfo.com.br/blog/projeto-sensor-jsn-sr04t-2-0-aj-sr04m-comunicacao-com-arduino/?srsltid=AfmBOopvsdUFvlvj2USKoAfYu04oJpcviJmg7YlTh83QradhTHyfNF62)
-* Microfone para capturar frequência chamando: [ref 1](https://circuitdigest.com/microcontroller-projects/how-to-detect-the-direction-of-sound-using-arduino)
+* Microfone para capturar frequência chamando: [ref 1](https://www.youtube.com/watch?v=59TqCV6DtKA), [ref 2](https://circuitdigest.com/microcontroller-projects/how-to-detect-the-direction-of-sound-using-arduino)
 * Extra: Controle remoto para controle manual
 
 ![Conexão articulada](./sketches/conexao-articulada.jpg)
@@ -30,6 +31,7 @@ Proposta de conexões com microfone, motor, sensor de distância e placa solar
 
 * 1 [Sensor de distância ultrassônico](https://www.mercadolivre.com.br/2x-sensor-ultrassnico-hc-sr04-de-distncia-para-arduino/p/MLB33346342?pdp_filters=item_id:MLB4512736584#polycard_client=recommendations_vip-pads-up&reco_backend=vip-pads-up-experimental-a_marketplace&wid=MLB4512736584&reco_model=rk_ent_v2_retsys_ads&reco_client=vip-pads-up&reco_item_pos=1&reco_backend_type=low_level&reco_id=ac27085a-d977-4706-8167-e5cd65d4a4f2&sid=recos&is_advertising=true&ad_domain=VIPDESKTOP_UP&ad_position=2&ad_click_id=Y2MwZGEzZGEtZGJiMy00YzIxLWEyMmYtYWJjMDliMTI3OTJj)
 * 1 [Microfone](https://produto.mercadolivre.com.br/MLB-3527016353-modulo-microfone-max4466-ganho-ajustavel-arduino-pic-esp-_JM#polycard_client=search-nordic&position=27&search_layout=stack&type=item&tracking_id=2258674f-dd40-4e4d-8389-b0f20a669a16&wid=MLB3527016353&sid=search)
+* 2 Motores DC + controlador L293D
 * 1 [Bomba de agua (pequena)](https://www.mercadolivre.com.br/mini-bomba-dagua-submersa-pra-arduino-3v-a-6v-dc-120l/p/MLB36183625?pdp_filters=item_id:MLB3774349427#wid=MLB3774349427&sid=search&is_advertising=true&searchVariation=MLB36183625&backend_model=search-backend&position=21&search_layout=grid&type=pad&tracking_id=271e353b-0d9a-455f-979f-0b4e5734c3d4&is_advertising=true&ad_domain=VQCATCORE_LST&ad_position=21&ad_click_id=OGI1ODRiYmMtODYwZC00YzczLTg1YTctZjMxM2ZmZWRlY2Fk)
 * 1 [Arduino UNO](https://produto.mercadolivre.com.br/MLB-3697545915-placa-compativel-uno-atmega328-ch340-cabo-usb-_JM?vip_filters=shipping%3Afulfillment&pdp_filters=shipping%3Afulfillment#polycard_client=search-nordic&position=25&search_layout=grid&type=item&tracking_id=cc0801a4-f82f-4d73-bb1e-b4bf85861dad&wid=MLB3697545915&sid=search)
 * 1 [Placa solar](https://produto.mercadolivre.com.br/MLB-3197175720-mini-painel-placa-solar-12v-15w-energia-fotovoltaica-celula-_JM#polycard_client=recommendations_vip-v2p&reco_backend=ranker_retrieval_online_vpp_v2p&reco_model=coldstart_high_exposition%2C+coldstart_low_exposition%2C+rk_online_v1_retsys_vpp_v2p&reco_client=vip-v2p&reco_item_pos=0&reco_backend_type=low_level&reco_id=5b90ecaf-89f2-4e3c-a35a-a18b5d9ecafb&wid=MLB3197175720&sid=recos)
--- sketch/sketch.ino
+++ sketch/sketch.ino
@@ -1,69 +1,43 @@
-
 /*
   Codigo para o trabalho Neosymbiota lucens 
   Descricao: Esse programa controla
   * Um sensor ultrassonico para medir distancia de obstaculo (e desviar)
   * Microfone para receber chamado (farol da atração). A frequencia padrao do chamado sera 3000 Hz.
   * Motores que serao acionados para desviar de obstaculos e ir em direcao ao chamado
-  Extras: Bluetooth para controle remoto via celular => 
+  * Bluetooth para controle remoto via celular  
 */
 
 #include "arduinoFFT.h"
-#include <Wire.h>
 #include <Adafruit_GFX.h>
 #include <Adafruit_SSD1306.h>
 #include <Dabble.h>
-#include <Servo.h>
+#define CUSTOM_SETTINGS
+#define INCLUDE_GAMEPAD_MODULE
+#include <AFMotor.h>
 
-// Sensor ultrassônico JSN-SR04T / AJ-SR04M.
-// a prova d'agua para medir distancia do obstáculo.
-#define ECHO 11  //  Pino ECHO do sensor esta conectado ao pino 11 do arduino.
-#define TRIG 12  // Pino TRIG do sensor esta conectado ao pino 12 do arduino.
-int dist = 0;    // distancia medida pelo sensor.
-long tempo;      // intervalo de tempo da reflezão da onda no sensor.
+// Expansor de pinos GPIO
+const int pcfAddress = 0x3F;
 
+/****************************************************
+ * Sensor ultrassônico JSN-SR04T / AJ-SR04M         *
+ * a prova d'agua para medir distancia do obstáculo *
+ *****************************************************/
+#define ECHO 11 //  Pino ECHO do sensor esta conectado ao pino 11 do arduino.
+#define TRIG 12 // Pino TRIG do sensor esta conectado ao pino 12 do arduino.
+int dist = 0;   // distancia medida pelo sensor.
+long tempo = 0; // intervalo de tempo da reflezão da onda no sensor.
 
-// Controle dos servo motores
-#define SERVO_ESQ 9
-#define SERVO_DIR 10
-Servo esq; // motor da esquerda
-Servo dir; // motor da direita
+/***************************
+ * Controle dos motores DC *
+ ***************************/
+AF_DCMotor motor3(3);  // motor da esquerda
+AF_DCMotor motor1(1);  // motor da direita
 
-void setup() {
-  // put your setup code here, to run once:
-  pinMode(ECHO, INPUT);   // pino ECHO do sensor esta configurado como entrada recebendo o sinal refletido.
-  pinMode(TRIG, OUTPUT);  // pino TRIG do sensor esta configurado como saida, acionando a emissao da onda.
-	esq.attach(SERVO_ESQ);
-	dir.attach(SERVO_DIR);
-  Serial.begin(9600);
-  Dabble.begin(9600);
-}
-
-void loop() {
-  // put your main code here, to run repeatedly:
-  medeDistancia();
-  ouveChamado();
-  leControle();
-}
-
-
-void medeDistancia() {
-  digitalWrite(TRIG, LOW);      // define pino TRIG como LOW para garantir que comece como zero.
-  delay(4);                     // espera 4 milissegundos antes de enviar o pulso.
-  digitalWrite(TRIG, HIGH);     // define pino TRIG como HIGH para enviar a onda ultrassonica.
-  delay(10);                    // espera 10 milissegundos para garantir que onda seja emitida.
-  digitalWrite(TRIG, LOW);      // define pino TRIG como LOW para finalizar emissao da onda.
-  tempo = pulseIn(ECHO, HIGH);  // pino ECHO e ativado quando o sinal refletido e recebido. pulseIn mede o tempo em que o pino ECHO esta em HIGH.
-  dist = (tempo * 0.034) / 2;    // calcula a distancia, considerando o tempo de ida e volta da onda.
-  Serial.print("Distancia medida: ");
-  Serial.print(dist);
-  Serial.println(" cm");
-  delay(50);
-}
-
-
-// Microfone Módulo Amplificador De Som Max9814.
-#define MIC1 A0
+/**********************************************
+ * Microfone Módulo Amplificador De Som LM393 *
+ *********************************************/
+#define MIC1_ANALOG A0
+#define MIC1_DIGITAL 7
 // A partir do sinal capturado pelo microfone, sera feita a transformada FFT (Fast Fourier Transform)
 // para analisar as frequencias e verificar se tem alto sinal na frequencia de 3000 Hz
 // Definicao dos parametros da transformada:
@@ -73,87 +47,155 @@ int SAMPLING_PERIOD_US = round(1000000 * (1.0 / SAMPLING_FREQ));
 // Objeto da transformada FFT e dados.
 double Real1[SAMPLES], Imag1[SAMPLES];
 ArduinoFFT<double> FFT1 = ArduinoFFT<double>(Real1, Imag1, SAMPLES, SAMPLING_FREQ, true);
-float targetFreq = 3000;  // frequencia de interesse.
+float targetFreq = 3000.0;  // frequencia de interesse.
 float magnitudeThreshold = 73.0;
+
+void setup() {
+	// put your setup code here, to run once:
+	pinMode(ECHO, INPUT);   // pino ECHO do sensor esta configurado como entrada recebendo o sinal refletido.
+	pinMode(TRIG, OUTPUT);  // pino TRIG do sensor esta configurado como saida, acionando a emissao da onda.
+	Serial.begin(9600);
+	Dabble.begin(9600);
+	// microfone
+	pinMode(MIC1_ANALOG, INPUT);
+	pinMode(MIC1_DIGITAL, INPUT);
+	// inicializacao dos motores
+	motor3.setSpeed(0);
+	motor1.setSpeed(0);
+}
+
+void loop() {
+	// put your main code here, to run repeatedly:
+	medeDistancia();
+	ouveChamado();
+	leControle();
+}
+
+
+void medeDistancia() {
+  digitalWrite(TRIG, LOW);      // define pino TRIG como LOW para garantir que comece como zero.
+  delay(10);                     // espera 4 milissegundos antes de enviar o pulso.
+  digitalWrite(TRIG, HIGH);     // define pino TRIG como HIGH para enviar a onda ultrassonica.
+  delay(10);                    // espera 10 milissegundos para garantir que onda seja emitida.
+  digitalWrite(TRIG, LOW);      // define pino TRIG como LOW para finalizar emissao da onda.
+  tempo = pulseIn(ECHO, HIGH);  // pino ECHO e ativado quando o sinal refletido e recebido. pulseIn mede o tempo em que o pino ECHO esta em HIGH.
+  dist = (tempo * 0.034) / 2;    // calcula a distancia, considerando o tempo de ida e volta da onda.
+  Serial.print("Distancia medida: ");
+  Serial.print(dist);
+  Serial.println(" cm");
+  delay(500);
+}
+
 
 // mapeamento da frequencia para FFT bin
 int frequencyToBin(float freq, float samplingFreq, int numSamples) {
-  return round((freq * numSamples) / samplingFreq);
+	return round((freq * numSamples) / samplingFreq);
 }
 
 void ouveChamado() {
-  long microseconds = micros();  // comeca tempo para coletar amostras.
-  for (int i = 0; i < SAMPLES; i++) {
-    Real1[i] = analogRead(MIC1);  // amostra do MIC1;
-    Imag1[i] = 0;
-  }
-  // espera tempo de coleta de amostras terminar.
-  while ((micros() - microseconds) < SAMPLING_PERIOD_US);
-  microseconds += SAMPLING_PERIOD_US;
-  // FFT.
-  FFT1.windowing(FFTWindow::Hamming, FFTDirection::Forward);
-  FFT1.compute(FFTDirection::Forward);
-  FFT1.complexToMagnitude();
-  // obtem as magnitudes para a frequencia de interesse.
-  int targetBin = frequencyToBin(targetFreq, SAMPLING_FREQ, SAMPLES);
-  float freqMag = Real1[targetBin];
-  if (freqMag > magnitudeThreshold) {
-    Serial.println("Tem alguem chamando");
-  }
+	long microseconds = micros();  // comeca tempo para coletar amostras.
+	for (int i = 0; i < SAMPLES; i++) {
+		Real1[i] = analogRead(MIC1_ANALOG);  // amostra do MIC1;
+		Serial.println("Real");
+		Serial.println(Real1[i]);
+		Imag1[i] = 0;
+		// espera tempo de coleta da amostra terminar.
+    while ((micros() - microseconds) < SAMPLING_PERIOD_US);
+    microseconds += SAMPLING_PERIOD_US;
+	}
+	// FFT.
+	FFT1.windowing(FFTWindow::Hamming, FFTDirection::Forward);
+	FFT1.compute(FFTDirection::Forward);
+	FFT1.complexToMagnitude();
+	// obtem as magnitudes para a frequencia de interesse.
+	int targetBin = frequencyToBin(targetFreq, SAMPLING_FREQ, SAMPLES);
+	float freqMag = Real1[targetBin];
+	if (freqMag > magnitudeThreshold) {
+		Serial.println("ANALOG: Tem alguem chamando");
+	// 		motor3.setSpeed(255);
+	// 		motor3.run(FORWARD);
+	// 		motor1.setSpeed(255);
+	// 		motor1.run(FORWARD);
+	} else {
+	// 		motor3.setSpeed(0);
+	// 		motor3.run(FORWARD);
+	// 		motor1.setSpeed(0);
+	// 		motor1.run(FORWARD);
+		Serial.println(freqMag);
+		Serial.println("ANALOG: Ninguem chamando");
+	}
+	delay(100);
+	bool digital = digitalRead(MIC1_DIGITAL);
+	if (digital == HIGH) {
+		Serial.println("DIGITAL HIGH: Tem alguem chamando");
+// 		motor3.setSpeed(255);
+// 		motor3.run(FORWARD);
+// 		motor1.setSpeed(255);
+// 		motor1.run(FORWARD);
+	} else {
+		Serial.println("digital low: ninguem chamando");
+	}
+	delay(500);
 }
 
 void leControle() {
-Dabble.processInput();
-	if(GamePad.isPressed(0)) {
-		esq.write(-360);
-		// esq.run(BACKWARD);
-		dir.write(360);
-		// dir.run(FORWARD);
-	}
-	else {
-		if(GamePad.isPressed(1)) {
-			esq.write(360);
-			// esq.run(FORWARD);
-			dir.write(-360);
-			// dir.run(BACKWARD);
-		}
-		else {
-			if(GamePad.isPressed(2)) {
-				esq.write(0);
-				// esq.run(BACKWARD);
-				dir.write(360);
-				// dir.run(FORWARD);
-			}
-			else {
-				if(GamePad.isPressed(3)) {
-					esq.write(-360);
-					// esq.run(BACKWARD);
-					dir.write(0);
-					// dir.run(FORWARD);
-				}
-				else {
-					if(GamePad.isPressed(9)) {
-						esq.write(360);
-						// esq.run(FORWARD);
-						dir.write(360);
-						// dir.run(FORWARD);
-					}
-					else {
-						if(GamePad.isPressed(7)) {
-							esq.write(-360);
-							// esq.run(BACKWARD);
-							dir.write(-360);
-							// dir.run(BACKWARD);
-						}
-						else {
-							esq.write(360);
-							// esq.run(BACKWARD);
-							dir.write(360);
-							// dir.run(FORWARD);
-						}
-					}
-				}
-			}
-		}
-	}
+	Dabble.processInput();
+	if (GamePad.isPressed(0)) {
+		// Serial.println("Pressionou 0");
+		motor3.setSpeed(255);
+		motor3.run(BACKWARD);
+		motor1.setSpeed(255);
+		motor1.run(FORWARD);
+		return;
+	} 
+	// pressionou seta para cima
+	if (GamePad.isPressed(1)) {
+		// Serial.println("Pressionou seta para cima (1)");
+		// motor3.setSpeed(255);
+		// motor3.run(FORWARD);
+		// motor1.setSpeed(255);
+		// motor1.run(BACKWARD);
+		return;
+	} 
+	// pressionou seta para baixo
+	if (GamePad.isPressed(2)) {
+	// 	Serial.println("Pressionou 2");
+	// 	motor3.setSpeed(0);
+	// 	motor3.run(BACKWARD);
+	// 	motor1.setSpeed(255);
+	// 	motor1.run(FORWARD);
+		return;
+	} 
+	// pressionou seta para esquerda
+	if (GamePad.isPressed(3)) {
+		Serial.println("Pressionou seta para esquerda (3)");
+		// motor3.setSpeed(255);
+		// motor3.run(BACKWARD);
+		// motor1.setSpeed(0);
+		// motor1.run(FORWARD);
+		return;
+	} 
+	// pressionou botao select
+	if (GamePad.isPressed(9)) {
+		Serial.println("Pressionou botão select (9)");
+		// motor3.setSpeed(255);
+		// motor3.run(FORWARD);
+		// motor1.setSpeed(255);
+		// motor1.run(FORWARD);
+		return;
+	} 
+	// pressionou quadrado
+	if (GamePad.isPressed(7)) {
+		Serial.println("Pressionou quadrado (7)");
+	// 	motor3.setSpeed(255);
+	// 	motor3.run(BACKWARD);
+	// 	motor1.setSpeed(255);
+	// 	motor1.run(BACKWARD);
+		return;
+	} 
+	motor3.setSpeed(0);
+	motor3.run(FORWARD);
+	// motor1.setSpeed(0);
+	// motor1.run(FORWARD);
+	// Serial.println("Nenhum botao apertado");
 }
