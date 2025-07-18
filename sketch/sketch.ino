869689fc20e42feb853a6ebd87ea7573be9441e4
Luisa Burini <lburini@unicamp.br>
2025-07-18 20:10:56 -0300

    :sparkles: adiciona outro motor e sensor ultrassonico

--- sketch/sketch.ino
+++ sketch/sketch.ino
@@ -21,8 +21,12 @@
  *****************************************************/
 #define ECHO1 11 //  Pino ECHO1 do sensor esta conectado ao pino 11 do arduino.
 #define TRIG1 12 // Pino TRIG1 do sensor esta conectado ao pino 12 do arduino.
+#define ECHO2 5 //  Pino ECHO2 do sensor esta conectado ao pino 5 do arduino.
+#define TRIG2 7 // Pino TRIG2 do sensor esta conectado ao pino 7 do arduino.
 int dist1 = 0;   // distancia medida pelo sensor.
-long tempo1 = 0; // intervalo de tempo1 da reflezão da onda no sensor.
+int dist2 = 0;   // distancia medida pelo sensor.
+long tempo1 = 0; // intervalo de tempo1 da reflexão da onda no sensor.
+long tempo2 = 0; // intervalo de tempo1 da reflexão da onda no sensor.
 bool obstaculo1Proximo = false;
 bool obstaculo2Proximo = false;
 
@@ -41,7 +45,6 @@ bool obstaculo2Proximo = false;
  * Microfone Módulo Amplificador De Som LM393 *
  *********************************************/
 #define MIC1_ANALOG A0
-#define MIC1_DIGITAL 7
 // A partir do sinal capturado pelo microfone, sera feita a transformada FFT (Fast Fourier Transform)
 // para analisar as frequencias e verificar se tem alto sinal na frequencia de 3000 Hz
 // Definicao dos parametros da transformada:
@@ -58,11 +61,12 @@ void setup() {
 	// put your setup code here, to run once:
 	pinMode(ECHO1, INPUT);   // pino ECHO1 do sensor esta configurado como entrada recebendo o sinal refletido.
 	pinMode(TRIG1, OUTPUT);  // pino TRIG1 do sensor esta configurado como saida, acionando a emissao da onda.
+	pinMode(ECHO2, INPUT);   // pino ECHO2 do sensor esta configurado como entrada recebendo o sinal refletido.
+	pinMode(TRIG2, OUTPUT);  // pino TRIG2 do sensor esta configurado como saida, acionando a emissao da onda.
 	Serial.begin(9600);
 	Dabble.begin(9600);
 	// microfone
 	pinMode(MIC1_ANALOG, INPUT);
-	pinMode(MIC1_DIGITAL, INPUT);
 	// inicializacao dos motores
 	pinMode(MOTOR1_IN1, INPUT);
 	pinMode(MOTOR1_IN2, INPUT);
@@ -71,17 +75,23 @@ void setup() {
 	pinMode(MOTOR2_IN2, INPUT);
 	pinMode(MOTOR2_ENABLE, INPUT);
 	desligaMotor1();
+	desligaMotor2();
 }
 
 void loop() {
 	// put your main code here, to run repeatedly:
-	mededistancia();
-	ouveChamado();
+	medeDistancia();
+	//ouveChamado();
 	//leControle();
 }
 
+void medeDistancia(){
+	medeDistancia1();
+	medeDistancia2();
+}
 
-void mededistancia() {
+
+void medeDistancia1() {
   digitalWrite(TRIG1, LOW);      // define pino TRIG1 como LOW para garantir que comece como zero.
   delay(10);                     // espera 4 milissegundos antes de enviar o pulso.
   digitalWrite(TRIG1, HIGH);     // define pino TRIG1 como HIGH para enviar a onda ultrassonica.
@@ -89,21 +99,43 @@ void mededistancia() {
   digitalWrite(TRIG1, LOW);      // define pino TRIG1 como LOW para finalizar emissao da onda.
   tempo1 = pulseIn(ECHO1, HIGH);  // pino ECHO1 e ativado quando o sinal refletido e recebido. pulseIn mede o tempo1 em que o pino ECHO1 esta em HIGH.
   dist1 = (tempo1 * 0.034) / 2;    // calcula a dist1ancia, considerando o tempo1 de ida e volta da onda.
-  Serial.print("distancia medida: ");
+  Serial.print("distancia 1 medida: ");
   Serial.print(dist1);
   Serial.println(" cm");
-	if (dist1 < 10) {
+	if (dist1 < 5) {
 		obstaculo1Proximo = true;
-		Serial.println("OBSTACULO MUITO PROXIMO");
+		Serial.println("OBSTACULO 1 MUITO PROXIMO");
 		ligaMotor1();
 		paraFrenteMotor1();
-		desligaMotor2();
 	} else {
 		obstaculo1Proximo = false;
-		Serial.println("OBSTACULO MUITO LONGE");
+		Serial.println("obstaculo 1 muito longe");
 		desligaMotor1();
+	}
+  delay(500);
+}
+
+
+void medeDistancia2() {
+  digitalWrite(TRIG2, LOW);      // define pino TRIG1 como LOW para garantir que comece como zero.
+  delay(10);                     // espera 4 milissegundos antes de enviar o pulso.
+  digitalWrite(TRIG2, HIGH);     // define pino TRIG1 como HIGH para enviar a onda ultrassonica.
+  delay(10);                    // espera 10 milissegundos para garantir que onda seja emitida.
+  digitalWrite(TRIG2, LOW);      // define pino TRIG1 como LOW para finalizar emissao da onda.
+  tempo2 = pulseIn(ECHO2, HIGH);  // pino ECHO1 e ativado quando o sinal refletido e recebido. pulseIn mede o tempo1 em que o pino ECHO1 esta em HIGH.
+  dist2 = (tempo2 * 0.034) / 2;    // calcula a dist1ancia, considerando o tempo1 de ida e volta da onda.
+  Serial.print("distancia 2 medida: ");
+  Serial.print(dist2);
+  Serial.println(" cm");
+	if (dist2 < 5) {
+		obstaculo2Proximo = true;
+		Serial.println("OBSTACULO 2 MUITO PROXIMO");
 		ligaMotor2();
 		paraFrenteMotor2();
+	} else {
+		obstaculo2Proximo = false;
+		Serial.println("obstaculo 2 muito longe");
+		desligaMotor2();
 	}
   delay(500);
 }
@@ -142,17 +174,8 @@ void ouveChamado() {
 		if (!obstaculo1Proximo){
 			desligaMotor1();
 		}
-	}
-	delay(500);
-	bool digital = digitalRead(MIC1_DIGITAL);
-	if (digital == HIGH) {
-		Serial.println("DIGITAL HIGH: TEM ALGUEM CHAMANDO");
-		ligaMotor1();
-		paraTrasMotor1();
-	} else {
-		Serial.println("digital low: ninguem chamando");
-		if (!obstaculo1Proximo){
-			desligaMotor1();
+		if (!obstaculo2Proximo) {
+			desligaMotor2();
 		}
 	}
 	delay(500);
@@ -214,10 +237,6 @@ void leControle() {
 		paraTrasMotor2();
 		return;
 	} 
-	// motor3.setSpeed(0);
-	// motor3.run(FORWARD);
-	// motor1.setSpeed(0);
-	// motor1.run(FORWARD);
 	Serial.println("Nenhum botao apertado");
 }
 
